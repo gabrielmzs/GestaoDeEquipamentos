69693c96301867e8a608b0638d0f95eea30cdfa3
Gabriel <mzsgabriel@gmail.com>
2023-04-03 11:18:32 -0300

    Ajustes Menus

--- GestaoDeEquipamentos/Program.cs
+++ GestaoDeEquipamentos/Program.cs
@@ -7,7 +7,7 @@ namespace GestaoDeEquipamentos {
         static ArrayList nomeEquipamento = new ArrayList() { "Nome", "Máquina", "Impressora", "Furadeira", "Sapato", "Lala" };
         static ArrayList precoAquisicao = new ArrayList() { "Preço", 300, 5000, 600, 300, 53 };
         static ArrayList numeroSerie = new ArrayList() { "Número de Série", 1413, 523, 5234, 6436, 2342 };
-        static ArrayList dataFabricacao = new ArrayList() { "Data de Fabricação", 23091999, 01052018, 07102015, 24102022, 0101 / 1998 };
+        static ArrayList dataFabricacao = new ArrayList() { "Data de Fabricação", "23/09/1999", "01/05/2018", "07/10/2015", "24/10/2022", "01/01/1998" };
         static ArrayList fabricante = new ArrayList() { "Fabricante", "machineTop", "Xerox", "Still", "CouroBom", "Telettubies" };
         static ArrayList chamado = new ArrayList() { "Chamado", "Cagada", "Cagada Gigante", "Deu Merda" };
         static ArrayList descChamado = new ArrayList() { "Descrição", "Deu ruim o negocio aqui", "Entupiram o vaso", "Cagaram na parede" };
@@ -34,20 +34,20 @@ namespace GestaoDeEquipamentos {
         }
         static void RegistrarEquip() {
             Console.Clear();
-            Console.WriteLine("Informe o nome do equipamento: ");
+            Console.Write("Informe o nome do equipamento: ");
             string nome = Console.ReadLine();
             while (nome.Length < 5) {
-                Console.WriteLine("O nome deve ter no mínimo 5 caracteres, digite novamente: ");
+                Console.Write("O nome deve ter no mínimo 5 caracteres, digite novamente: ");
                 nome = Console.ReadLine();
             }
             nomeEquipamento.Add(nome);
-            Console.WriteLine("Informe o preço de aquisição: ");
+            Console.Write("Informe o preço de aquisição: ");
             precoAquisicao.Add(Console.ReadLine());
-            Console.WriteLine("Informe o número de série: ");
+            Console.Write("Informe o número de série: ");
             numeroSerie.Add(Console.ReadLine());
-            Console.WriteLine("Informe a data de fabricação (dd/mm/aaaa): ");
+            Console.Write("Informe a data de fabricação (dd/mm/aaaa): ");
             dataFabricacao.Add(Console.ReadLine());
-            Console.WriteLine("Informe o fabricante: ");
+            Console.Write("Informe o fabricante: ");
             fabricante.Add(Console.ReadLine());
 
         }
@@ -86,22 +86,15 @@ namespace GestaoDeEquipamentos {
             fabricante.RemoveAt(indice);
         }
         static void RegistrarChamado() {
-            Console.WriteLine($"Indíce".PadRight(10) + $"|{nomeEquipamento[0]}".PadRight(30) + $"|{precoAquisicao[0]}".PadRight(20) + $"|{numeroSerie[0]}".PadRight(20) + $"|{dataFabricacao[0]}".PadRight(20) + $"|{fabricante[0]}".PadRight(20));
-            Console.WriteLine("------------------------------------------------------------------------------------------------------------------------");
-            for (int i = 1; i < nomeEquipamento.Count; i++) {
-                Console.WriteLine($"{i}".PadRight(10) + $"|{nomeEquipamento[i]}".PadRight(30) + $"|{precoAquisicao[i]}".PadRight(20) + $"|{numeroSerie[i]}".PadRight(20) + $"|{dataFabricacao[i]}".PadRight(20) + $"|{fabricante[i]}".PadRight(20));
-
-            }
-            Console.WriteLine("Informe o indíce do equipamento do chamado: ");
+            ImprimirEquip();
+            Console.Write("Informe o indíce do equipamento do chamado: ");
             int equipamento = int.Parse(Console.ReadLine());
             indiceEquip.Add(nomeEquipamento[equipamento]);
-            Console.Clear();
-            Console.ReadLine();
-            Console.WriteLine("Informe o título do chamado: ");
+            Console.Write("Informe o título do chamado: ");
             chamado.Add(Console.ReadLine());
-            Console.WriteLine("Informe a descrição do chamado: ");
+            Console.Write("Informe a descrição do chamado: ");
             descChamado.Add(Console.ReadLine());
-            Console.WriteLine("Informe a data de abertura do chamado: ");
+            Console.Write("Informe a data de abertura do chamado: ");
             DateTime data = Convert.ToDateTime(Console.ReadLine());
             aberturaChamado.Add(data);
         }
@@ -118,23 +111,23 @@ namespace GestaoDeEquipamentos {
         }
         static void EditarChamado() {
             EscreverChamado();
-            Console.WriteLine("Informe o indíce do chamado a ser Editado:");
+            Console.Write("Informe o indíce do chamado a ser Editado:");
             int indice = int.Parse(Console.ReadLine());
-
-            Console.WriteLine("Informe o indíce do equipamento do chamado: ");
+            ImprimirEquip();
+            Console.Write("\nInforme o indíce do equipamento do chamado: ");
             int equipamento = int.Parse(Console.ReadLine());
             indiceEquip[indice] = nomeEquipamento[equipamento];
-            Console.WriteLine("Informe o título do chamado: ");
+            Console.Write("Informe o título do chamado: ");
             chamado[indice] = Console.ReadLine();
-            Console.WriteLine("Informe a descrição do chamado: ");
+            Console.Write("Informe a descrição do chamado: ");
             descChamado[indice] = Console.ReadLine();
-            Console.WriteLine("Informe a data de abertura do chamado: ");
+            Console.Write("Informe a data de abertura do chamado: ");
             DateTime data = Convert.ToDateTime(Console.ReadLine());
             aberturaChamado[indice] = data;
         }
         static void ExcluirChamado() {
             EscreverChamado();
-            Console.WriteLine("Informe o indíce do chamado a ser Excluído:");
+            Console.Write("Informe o indíce do chamado a ser Excluído:");
             int indice = int.Parse(Console.ReadLine());
 
             indiceEquip.RemoveAt(indice);
